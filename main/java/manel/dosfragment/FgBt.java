e08c6ca08804ebed953dde376a58a51e81ec5ec7
Jorge Otero del Río email = joterodelrio@danielcastelao.org <joterodelrio@danielcastelao.org>
2017-06-13 19:24:40 +0200

    Falta landscape

---
+++ main/java/manel/dosfragment/FgBt.java
@@ -0,0 +1,29 @@
+package manel.dosfragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FgBt extends Fragment {
+
+
+    public FgBt() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fg_bt, container, false);
+    }
+
+}
---
+++ main/java/manel/dosfragment/MainActivity.java
@@ -0,0 +1,19 @@
+package manel.dosfragment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void saltoVentana(View v){
+        Intent i = new Intent(getBaseContext(),Main2Activity.class);
+        startActivity(i);
+    }
+}
